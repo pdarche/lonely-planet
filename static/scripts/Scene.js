877efcd31a84d27d63435c88f7710cf21fc363df
Peter Darche <pdarche@gmail.com>
2013-01-05 20:08:05 -0500

    organizationl changes.  start making js more oo

---
+++ static/scripts/Scene.js
@@ -0,0 +1,58 @@
+var Scene = {
+
+	init	: function(){
+
+		Scene.WIDTH  = window.innerWidth,
+		Scene.HEIGHT = window.innerHeight
+
+		//scene
+		Scene.scene = new THREE.Scene()
+
+		//camera
+		Scene.VIEW_ANGLE = 45,
+		Scene.ASPECT     = WIDTH / HEIGHT,
+		Scene.NEAR       = 0.1,
+		Scene.FAR        = 10000
+
+		Scene.camera = new THREE.PerspectiveCamera( Scene.VIEW_ANGLE, Scene.ASPECT, Scene.NEAR, Scene.FAR )
+		Scene.camera.position.set( 0, 0, 600 )
+		Scene.camera.lookAt( Scene.scene.position )
+		Scene.scene.add( Scene.camera )
+
+		//shaders
+		Scene.shader = THREE.ShaderUtils.lib[ "normal" ];
+		Scene.uniforms = THREE.UniformsUtils.clone( shader.uniforms );
+
+		Scene.uniforms[ "tNormal" ].value = normalTexture;
+		Scene.uniforms[ "uNormalScale" ].value.set( 0.85, 0.85 );
+
+		Scene.uniforms[ "tDiffuse" ].value = planetTexture, cloudsTexture;
+		Scene.uniforms[ "tSpecular" ].value = specularTexture;
+
+		Scene.uniforms[ "enableAO" ].value = false;
+		Scene.uniforms[ "enableDiffuse" ].value = true;
+		Scene.uniforms[ "enableSpecular" ].value = true;
+
+		Scene.uniforms[ "uDiffuseColor" ].value.setHex( 0xffffff );
+		Scene.uniforms[ "uSpecularColor" ].value.setHex( 0x333333 );
+		Scene.uniforms[ "uAmbientColor" ].value.setHex( 0xffffff );
+
+		Scene.uniforms[ "uShininess" ].value = 15;
+
+		Scene.parameters = {
+
+			fragmentShader: shader.fragmentShader,
+			vertexShader: shader.vertexShader,
+			uniforms: uniforms,
+			lights: true,
+			fog: true
+
+		};
+
+		Scene.materialNormalMap = new THREE.ShaderMaterial( Scene.parameters );
+
+
+	},
+
+
+}
---
+++ twitstream.py
@@ -0,0 +1,98 @@
+import sys
+import getpass
+import itertools
+from optparse import OptionParser, OptionGroup
+from functools import partial
+from collections import defaultdict
+from twittornado import TwitterStreamGET, TwitterStreamPOST
+
+USAGE = """%prog [options] method [params]
+
+Public methods are 'spritzer', 'follow', and 'track'. Follow takes
+user IDs as parameters, and track takes keywords."""
+
+GETMETHODS  = ['firehose',
+               'gardenhose',
+               'spritzer',
+               'retweet',
+               'links',
+               'user',]
+
+POSTPARAMS  = {'birddog': 'follow',
+               'shadow':  'follow',
+               'follow':  'follow',
+               'track':   'track',}
+
+def constant_factory(value):
+    return itertools.repeat(value).next
+
+BASEURL = defaultdict(constant_factory("https://stream.twitter.com/%s.json"))
+BASEURL['user'] = "https://chirpstream.twitter.com/%s.json"
+
+METHODPATH   = {
+                'firehose':   '1/statuses/firehose',
+                'gardenhose': '1/statuses/sample',
+                'spritzer':   '1/statuses/sample',
+                'birddog':    '1/statuses/filter',
+                'shadow':     '1/statuses/filter',
+                'follow':     '1/statuses/filter',
+                'track':      '1/statuses/filter',
+                'filter':     '1/statuses/filter',
+                'retweet':    '1/statuses/retweet',
+                'links':      '1/statuses/links',
+                'user':       '2b/user',
+                }
+
+def DEFAULTACTION(status):
+    if "user" not in status:
+        try:
+            if options.debug:
+                print >> sys.stderr, status
+            return
+        except:
+            pass
+    print "%s:\t%s\n" % (status.get('user', {}).get('screen_name'), status.get('text'))
+
+def twitstream(method, user, pword, action, defaultdata=[], debug=False, engine='async', **kwargs):
+    '''General function to set up an asynchat object on twitter. Chooses GET or
+    POST according to the API method.
+    
+    Parameter action is a callable that takes a dict derived from simplejson.
+    
+    Parameter defaultdata expects an iterable of strings as the default parameter 
+    (follow or track) on a POST method. If there are additional parameters you
+    wish to use, they can be passed in **kwargs.'''
+    
+    url = BASEURL[method] % METHODPATH[method]
+
+    data = {POSTPARAMS[method]: ','.join(defaultdata)}
+    data.update(kwargs)
+    return TwitterStreamPOST(user, pword, url, action, data, debug)
+     
+
+track = partial(twitstream, 'track')
+
+track.__doc__    = "receive all real-time mentions of any of the input terms"
+
+parser = OptionParser(usage=USAGE)
+group = OptionGroup(parser, 'credentials',
+                    "All usage of the Streaming API requires user credentials. "
+                    "Will prompt if either of them are missing.")
+group.add_option('-p', '--password', help="Twitter password")
+group.add_option('-u', '--username', help="Twitter username")
+parser.add_option_group(group)
+parser.add_option('--debug', action='store_true', dest='debug', 
+                    default=False, help="Print debug information")
+egroup = OptionGroup(parser, 'engine',
+                    "Selects the underlying library for asyncronous I/O.")
+egroup.add_option('--tornado', action='store_const', const='tornado', 
+                  dest='engine', help="Use Tornado's iostream library")
+parser.add_option_group(egroup)
+
+
+def ensure_credentials(options):
+    if not options.username:
+        options.username = raw_input("Twitter username: ")
+    if not options.password:
+        options.password = getpass.getpass(prompt='Password for %s: ' % options.username)
+    return options
