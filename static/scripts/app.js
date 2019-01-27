ca3b7d3a73da3513f53acf6dfb4c2f5e7e7fbcd3
Peter Darche <pdarche@gmail.com>
2019-01-26 17:56:56 -0800

    Revert host to .me

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -41,7 +41,7 @@ $(document).ready(function(){
   // event delegator for inter-view event handling
   vent = _.extend({}, Backbone.Events);
   // socket connection to the server
-  socket = new WebSocket("wss://lonelyplanet.darche.pizza/socket");
+  socket = new WebSocket("wss://lonelyplanet.darche.me/socket");
   // the collection of tweets
   tweetsCollection = new app.TweetsCollection();
   // The main three.js scene
